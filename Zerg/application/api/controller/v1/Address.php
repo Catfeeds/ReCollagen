0c9a124b317cbf2ab7d248d8e854e497a80d615e
newbie91521 <newbie91521@163.com>
2017-12-18 11:06:57 +0800

    1

--- Zerg/application/api/controller/v1/Address.php
+++ Zerg/application/api/controller/v1/Address.php
@@ -1,15 +1,6 @@
 <?php
-/**
- * Created by 七月.
- * Author: 七月
- * 微信公号：小楼昨夜又秋风
- * 知乎ID: 七月在夏天
- * Date: 2017/2/23
- * Time: 2:56
- */
 
 namespace app\api\controller\v1;
-
 
 use app\api\controller\BaseController;
 use app\api\model\User;
@@ -59,7 +50,7 @@ class Address extends BaseController
         if(!$user){
             throw new UserException([
                 'code' => 404,
-                'msg' => '用户收获地址不存在',
+                'msg' => '用户收货地址不存在',
                 'errorCode' => 60001
             ]);
         }
