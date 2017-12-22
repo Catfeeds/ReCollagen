8f9f1d370b9bd7a99ac60860cac75e1f76b44f59
newbie91521 <newbie91521@163.com>
2017-12-22 11:11:51 +0800

    1

--- Zerg/application/api/model/UserAddress.php
+++ Zerg/application/api/model/UserAddress.php
@@ -7,9 +7,19 @@ use think\Model;
 class UserAddress extends BaseModel
 {
     protected $table = 'osc_address';
+    protected $autoWriteTimestamp = true;
+    protected $hidden =['address_id','create_time','update_time','province_id'];
 
-    protected $hidden =['address_id', 'update_time'];
+    public static function getProvinceId($province){
+        $provinces = Area::all(['area_parent_id'=>0]);
+        foreach ($provinces as $key => $v) {
+            if (strpos($province, $v['area_name']) !== false) {
+                return $v['area_id'];
+                break;
+            }
+        }
 
+    }
 }
 
 
--- Zerg/application/api/validate/AddressNew.php
+++ Zerg/application/api/validate/AddressNew.php
@@ -10,11 +10,11 @@ class AddressNew extends BaseValidate
     // 获取post参数时过滤掉user_id
     // 所有数据库和user关联的外键统一使用user_id，而不要使用uid
     protected $rule = [
-        'name' => 'require|isNotEmpty',
-        'mobile' => 'require|isMobile',
-        'region' => 'require|isNotEmpty',
-        // 'city' => 'require|isNotEmpty',
-        // 'country' => 'require|isNotEmpty',
-        'detail' => 'require|isNotEmpty',
+        'name'      => 'require|isNotEmpty',
+        'telephone' => 'require|isMobile',
+        'province'  => 'require|isNotEmpty',
+        'city'      => 'require|isNotEmpty',
+        'country'   => 'require|isNotEmpty',
+        'address'   => 'require|isNotEmpty',
     ];
 }
