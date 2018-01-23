e5ce757542081c9569e4b2e708152e4954ced4e3
newbie91521 <newbie91521@163.com>
2018-01-23 17:20:13 +0800

    1

--- Protoss/pages/userinfo/userinfo-model.js
+++ Protoss/pages/userinfo/userinfo-model.js
@@ -22,6 +22,22 @@ class UserInfo extends Base{
       this.request(param);
     }
 
+    /*上传图片*/
+    _updateUserInfo(data, callback) {
+      var param = {
+        url: 'user/update',
+        type: 'post',
+        data: data,
+        sCallback: function (res) {
+          callback && callback(res);
+        },
+        eCallback(res) {
+          callback && callback(res);
+        }
+      };
+      this.request(param);
+    }
+
     /*获取我的账户所有信息*/
     getUserAccount(callback) {
       var param = {
--- Protoss/pages/userinfo/userinfo.js
+++ Protoss/pages/userinfo/userinfo.js
@@ -11,6 +11,9 @@ Page({
       IDcode: '',
       up_name: '',
       up_wecat: '',
+      IDcode_pic:'../../imgs/icon/IDcode_pic_b.png',
+      IDcode_pic_b: null,
+      IDcode_pic_h: null,
     },
     loadingHidden: false,
     sexArray: ['男', '女'],
@@ -128,6 +131,39 @@ Page({
   bindUpWecat(e) {
     this.setData({
       'userData.up_wecat': e.detail.value
+    })
+  },
+
+  chooseImageTap: function () {
+    let _this = this;
+    wx.showActionSheet({
+      itemList: ['从相册中选择', '拍照'],
+      itemColor: "#f7982a",
+      success: function (res) {
+        if (!res.cancel) {
+          if (res.tapIndex == 0) {
+            _this.chooseWxImage('album')
+          } else if (res.tapIndex == 1) {
+            _this.chooseWxImage('camera')
+          }
+        }
+      }
+    })
+
+  },
+  chooseWxImage: function (type) {
+    let _this = this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'],
+      sourceType: [type],
+      success: function (res) {
+        console.log(res);
+
+        _this.setData({
+          logo: res.tempFilePaths[0],
+        })
+
+      }
     })
   },
 
