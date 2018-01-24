a3c51879d960dc312f61eb01948e6d8c02f23d09
newbie91521 <newbie91521@163.com>
2018-01-24 11:07:18 +0800

    1

---
+++ Protoss/pages/order-details/order-details-model.js
@@ -0,0 +1,103 @@
+/**
+ * Created by jimmy on 17/03/09.
+ */
+
+import {Base} from '../../utils/base.js'
+
+class OrderDetails extends Base{
+
+    constructor(){
+        super();
+    }
+
+    /*
+    * 余额、小金库支付
+    * params:
+    * norderNumber - {int} 订单id
+    * */
+    execPay(orderNumber,callback){
+        var allParams = {
+            url: 'pay/pre_order',
+            type:'post',
+            data:{id:orderNumber},
+            sCallback: function (data) {
+              callback && callback(data);
+            },
+            eCallback: function (data) {
+              callback && callback(data);
+            }
+        };
+        this.request(allParams);
+    }
+
+    /*
+    * 取消订单
+    * params:
+    * norderNumber - {int} 订单id
+    * */
+    cancel(orderNumber, callback) {
+      var allParams = {
+        url: 'order/cancel',
+        type: 'post',
+        data: { id: orderNumber },
+        sCallback: function (data) {
+          callback && callback(data);
+        },
+        eCallback: function (data) {
+          callback && callback(data);
+        }
+      };
+      this.request(allParams);
+    }
+
+    /*
+    * 确认收货
+    * params:
+    * norderNumber - {int} 订单id
+    * */
+    receive(orderNumber, callback) {
+      console.log(orderNumber)
+      var allParams = {
+        url: 'order/receive',
+        type: 'post',
+        data: { id: orderNumber },
+        sCallback: function (data) {
+          callback && callback(data);
+        },
+        eCallback: function (data) {
+          callback && callback(data);
+        }
+      };
+      this.request(allParams);
+    }
+
+    /*获得所有订单,pageIndex 从1开始*/
+    getOrders(statusId,pageIndex,callback){
+        var allParams = {
+            url: 'order/by_user/' + statusId+'/'+pageIndex,
+            type:'get',
+            sCallback: function (data) {
+              callback && callback(data);  //1待付款,2待发货,3已发货,4已收货,5已取消订单
+             }
+        };
+        this.request(allParams);
+    }
+
+    /*获得订单的具体内容*/
+    getOrderInfo(id,callback){
+        var that=this;
+        var allParams = {
+            url: 'order/'+id,
+            sCallback: function (data) {
+                callback &&callback(data);
+            },
+            eCallback:function(){
+              
+            }
+        };
+        this.request(allParams);
+    }
+    
+}
+
+export { OrderDetails};
---
+++ Protoss/pages/order-details/order-details.js
@@ -0,0 +1,158 @@
+import { OrderDetails } from 'order-details-model.js';
+import {Cart} from '../cart/cart-model.js';
+import { AdrList } from '../address/list/list-model.js';
+
+var order = new OrderDetails();
+var cart=new Cart();
+var adrList = new AdrList();
+
+Page({
+        data: {
+            addressInfo:null
+        },
+
+        onLoad: function (options) {
+            this.data.id=options.id;
+        },
+
+        onShow:function(){
+          var that = this;
+            if(this.data.id) {
+                var id = this.data.id;
+                order.getOrderInfo(id, (data)=> {
+                    that.setData({
+                        orderStatus: data.order_status,
+                        productsArr: data.products,
+                        basicInfo: {
+                            orderTime: data.create_time,
+                            orderNo: data.order_num_alias
+                        },
+                        wuliuInfo: {
+                          orderMethod: data.shipping_method,
+                          orderNum: data.shipping_num
+                        },
+                        addressInfo: {
+                          name: data.shipping_name,
+                          telephone: data.shipping_tel,
+                          address: data.shipping_addr,
+                        },
+                        PromotionInfo: data.promotionName,
+                        mainGoodsPrice: data.mainGoodsPrice,
+                        otherGoodsPrice: data.otherGoodsPrice,
+                        shippingPrice: data.shippingPrice,
+                        total: data.total,
+                    });
+                });
+            }
+        },
+        
+        /*查看物流*/
+        showOrderWul: function (event) {
+          var id = this.data.id;
+          wx.navigateTo({
+            url: '../transinfo/transinfo?id=' + id
+          });
+        },
+
+        /*取消订单*/
+        cancel: function (event) {
+          var that = this,
+            id = this.data.id;
+          this.showTipsReturn('提示', '你确定要取消订单吗？', (statusConfirm) => {
+            if (statusConfirm) {
+              order.cancel(id, (statusCode) => {
+                if (statusCode.errorCode != 0) {
+                  that.showTips('订单提示', statusCode.msg);
+                  return;
+                }
+                that.setData({
+                  orderStatus: 5
+                });
+              });
+            }
+          })
+        },
+
+        /*确认收货*/
+        receive: function (event) {
+          var that = this,
+            id = this.data.id;
+          this.showTipsReturn('提示', '你确认要收货吗？', (statusConfirm) => {
+            if (statusConfirm) {
+              order.receive(id, (statusCode) => {
+                if (statusCode.errorCode != 0) {
+                  that.showTips('订单提示', statusCode.msg);
+                  return;
+                }
+                that.setData({
+                  orderStatus: 4
+                });
+              });
+            }
+          });
+        },
+
+        /*付款*/
+        pay:function(){
+            this._execPay(this.data.id);
+        },
+
+        /*
+        *开始支付
+        * params:
+        * id - {int}订单id
+        */
+        _execPay:function(id){
+            var that=this;
+            order.execPay(id,(statusCode)=>{
+              if (statusCode.errorCode != 0) {
+                that.showTips('支付提示', statusCode.msg);
+                return;
+              }
+              wx.navigateTo({
+                  url: '../pay-result/pay-result?id=' + id + '&from=order'
+              });
+            });
+        },
+
+        /*
+        * 提示窗口
+        * params:
+        * title - {string}标题
+        * content - {string}内容
+        * flag - {bool}是否跳转到 "我的页面"
+        */
+        showTips: function (title, content, flag) {
+          wx.showModal({
+            title: title,
+            content: content,
+            showCancel: false,
+            success: function (res) {
+              if (flag) {
+                wx.switchTab({
+                  url: '/pages/my/my'
+                });
+              }
+            }
+          });
+        },
+
+        /*
+        * 提示窗口 - 返回值
+        * params:
+        * title - {string}标题
+        * content - {string}内容
+        * callback - {bool}返回值
+        */
+        showTipsReturn: function (title, content, callback) {
+          wx.showModal({
+            title: title,
+            content: content,
+            showCancel: true,
+            success: function (res) {
+              callback && callback(res.confirm);
+            }
+          });
+        },
+    }
+)
